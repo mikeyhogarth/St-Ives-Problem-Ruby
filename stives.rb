21e4e271a07a6f16a544238a4864ce9dfe12e62d
Mikey Hogarth <mikehogarth20@hotmail.com>
2011-11-29 18:50:00 +0000

    removed curly braces from hashes

--- stives.rb
+++ stives.rb
@@ -4,9 +4,9 @@
 require "some-facts" 
 
 as_i_was_going_to :st_ives
-i_met_a :man, { with(:seven) => :wives }
-every :wife,  { had(:seven) => :sacks }
-every :sack,  { had(:seven) => :cats }
-every :cat,   { had(:seven) => :kits }
+i_met_a :man,  with(:seven) => :wives 
+every :wife, had(:seven) => :sacks 
+every :sack, had(:seven) => :cats 
+every :cat, had(:seven) => :kits 
 so :kits, :cats, :sacks, :wives
 how_many_were_going_to :st_ives
